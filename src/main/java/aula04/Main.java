471726a7eb1eba6234842d297d69c1d97e3cabfd
Millene Toledo <escolamillene.drive@gmail.com>
2026-03-09 20:27:40 -0300

    aula 04, exemplo do professor

---
+++ src/main/java/aula04/Main.java
@@ -0,0 +1,13 @@
+package aula04;
+
+public class Main {
+    public static void main(String[] args){
+        Person Vinicius = new Person();
+
+        Vinicius.name = "Vinicius Savio Medina";
+        Vinicius.age = 20;
+
+        Vinicius.talk();
+    }
+
+}
---
+++ src/main/java/aula04/Person.java
@@ -0,0 +1,10 @@
+package aula04;
+
+public class Person {
+    int age;
+    String name;
+
+    void talk(){
+        System.out.println("Hello! My name is " + name + " and i am " + age + " years old." );
+    }
+}
